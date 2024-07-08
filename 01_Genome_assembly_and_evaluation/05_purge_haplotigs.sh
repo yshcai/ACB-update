f2b9d7c0b938096d33c219d7bf791ee9ff772112
Enosh <79687590+yshcai@users.noreply.github.com>
2024-07-08 16:47:32 +0800

    Update 05_purge_haplotigs.sh

--- 01_Genome_assembly_and_evaluation/05_purge_haplotigs.sh
+++ 01_Genome_assembly_and_evaluation/05_purge_haplotigs.sh
@@ -3,14 +3,17 @@ fasta=/public/caiyuanshi/hifi/t2t_of/2.Assembly/contig_fasta/hifi_only_s0.25/Of_
 ccs=/public/caiyuanshi/hifi/of_genome/1.HiFireads/Of_FP283.ccs.fastq.gz
 tmp=/public/caiyuanshi/hifi/t2t_of/2.Assembly/contig_fasta/hifi_only_s0.25/purge_haplotigs/tmp
 
-# map hifi reads to genome
-#minimap2 -t 24 -ax map-hifi $fasta $ccs --secondary=no | samtools view -bS - | samtools sort -@ 4 -m 1G -O BAM -o 01.minimap2/aligned.bam -T $tmp/tmp.ali -
+##### activate conda environment #####
+source /public/caiyuanshi/miniconda3/bin/activate purge_haplotigs
 
-# generate a coverage histogram
-#purge_haplotigs hist -b 01.minimap2/aligned.bam -g $fasta
+##### map hifi reads to genome #####
+minimap2 -t 24 -ax map-hifi $fasta $ccs --secondary=no | samtools view -bS - | samtools sort -@ 4 -m 1G -O BAM -o 01.minimap2/aligned.bam -T $tmp/tmp.ali -
 
-# contigs converage stats
+##### generate a coverage histogram #####
+purge_haplotigs hist -b 01.minimap2/aligned.bam -g $fasta
+
+##### contigs converage stats #####
 purge_haplotigs cov -i 02.cov/aligned.bam.gencov -l 5 -m 36 -h 110
 
-# purge
+##### purge #####
 purge_haplotigs purge -g $fasta -c coverage_stats.csv -d -b 01.minimap2/aligned.bam
