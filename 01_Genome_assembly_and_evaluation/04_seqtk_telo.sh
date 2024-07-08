bd9052972b55095e5372a4a13908c2033c91ae64
Enosh <79687590+yshcai@users.noreply.github.com>
2024-07-08 16:40:51 +0800

    Update 04_seqtk_telo.sh

--- 01_Genome_assembly_and_evaluation/04_seqtk_telo.sh
+++ 01_Genome_assembly_and_evaluation/04_seqtk_telo.sh
@@ -1,3 +1,7 @@
 # !/bin/bash
 
+##### activate conda environment #####
+source /public/caiyuanshi/miniconda3/bin/activate seqtk
+
+##### detect telomeric regions #####
 seqtk telo -m CCTAA -s 1 Of_FP283.asm.bp.p_ctg.gfa.fa > Of_FP283.asm.bp.p_ctg.gfa.fa.telo.bed 2> Of_FP283.asm.bp.p_ctg.gfa.fa.telo.count
