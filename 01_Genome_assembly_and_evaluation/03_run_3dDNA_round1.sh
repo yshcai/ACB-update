0c38445f16fe2fed1d8262b515d1426234dae278
Enosh <79687590+yshcai@users.noreply.github.com>
2024-07-08 16:10:54 +0800

    Create 03_run_3dDNA_round1.sh

---
+++ 01_Genome_assembly_and_evaluation/03_run_3dDNA_round1.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+juicer=/public/caiyuanshi/hifi/t2t_of/2.Assembly/contig_fasta/hifi_only_s0.25/juicer
+
+if [ ! -d ${juicer%/*}/3dDNA ];then
+        mkdir -p ${juicer%/*}/3dDNA
+fi
+references=$juicer/references
+chr=${juicer%/*}/3dDNA
+cd $chr
+/public/caiyuanshi/bioinfo_software/3dDNA/3d-dna-master/run-asm-pipeline.sh --input 10000 --early-exit \
+$references/${fasta##*/} $juicer/work/FPH2/aligned/merged_nodups.txt &>3dDNA_run.log
