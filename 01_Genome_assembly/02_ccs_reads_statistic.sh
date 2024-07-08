9155e9a597eb11501ca4a76d0906288c69ab0cc6
Enosh <79687590+yshcai@users.noreply.github.com>
2024-07-08 11:32:04 +0800

    Update 02_ccs_reads_statistic.sh

--- 01_Genome_assembly/02_ccs_reads_statistic.sh
+++ 01_Genome_assembly/02_ccs_reads_statistic.sh
@@ -1,8 +1,11 @@
 # !/bin/bash
 hifireads=/public/caiyuanshi/hifi/of_genome/1.HiFireads/Of_FP283.ccs.fastq.gz
 
+##### activate conda environment #####
+soruce /public/caiyuanshi/miniconda3/bin/activate seqkit
+
 ##### simple statistics of FASTA/Q files #####
 #seqkit stats -a -j 2 $hifireads > hifireads_stats.txt
 
-##### convert FASTA/Q to tabular format (and length, GC content, average quality...) #####
+##### convert FASTA/Q to tabular format (and length, GC content ...) #####
 seqkit fx2tab --length --gc --only-id --name -j 2 $hifireads -o Of_FB283.ccs.fastq.Length_GC.txt
