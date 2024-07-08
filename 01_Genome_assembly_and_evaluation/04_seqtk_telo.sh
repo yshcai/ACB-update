eac8cd3d7bd529db63f82f9310b8f804a5d8ff4f
Enosh <79687590+yshcai@users.noreply.github.com>
2024-07-08 19:16:56 +0800

    Update 04_seqtk_telo.sh

--- 01_Genome_assembly_and_evaluation/04_seqtk_telo.sh
+++ 01_Genome_assembly_and_evaluation/04_seqtk_telo.sh
@@ -1,7 +1,8 @@
 # !/bin/bash
+fasta=/public/caiyuanshi/hifi/t2t_of/2.Assembly/contig_fasta/hifi_only_s0.25/Of_FP283.asm.bp.p_ctg.gfa.fa
 
 ##### activate conda environment #####
 source /public/caiyuanshi/miniconda3/bin/activate seqtk
 
 ##### detect telomeric regions #####
-seqtk telo -m CCTAA -s 1 Of_FP283.asm.bp.p_ctg.gfa.fa > Of_FP283.asm.bp.p_ctg.gfa.fa.telo.bed 2> Of_FP283.asm.bp.p_ctg.gfa.fa.telo.count
+seqtk telo -m CCTAA -s 1 $fasta > Of_FP283.asm.bp.p_ctg.gfa.fa.telo.bed 2> Of_FP283.asm.bp.p_ctg.gfa.fa.telo.count
