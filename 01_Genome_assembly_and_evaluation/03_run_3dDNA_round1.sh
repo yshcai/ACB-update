111bf2c1469da9d05792bf82deeedd2a26cd6faa
Enosh <79687590+yshcai@users.noreply.github.com>
2024-07-08 16:39:21 +0800

    Update 03_run_3dDNA_round1.sh

--- 01_Genome_assembly_and_evaluation/03_run_3dDNA_round1.sh
+++ 01_Genome_assembly_and_evaluation/03_run_3dDNA_round1.sh
@@ -1,6 +1,10 @@
 # !/bin/bash
 juicer=/public/caiyuanshi/hifi/t2t_of/2.Assembly/contig_fasta/hifi_only_s0.25/juicer
 
+##### activate conda environment #####
+source /public/caiyuanshi/miniconda3/bin/activate juicer
+
+##### 3dDNA round 1 #####
 if [ ! -d ${juicer%/*}/3dDNA ];then
         mkdir -p ${juicer%/*}/3dDNA
 fi
