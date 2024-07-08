e44b972d236e6c63d4814e63acee05eca29431da
Enosh <79687590+yshcai@users.noreply.github.com>
2024-07-08 15:07:34 +0800

    Update 04_hic_reads_statistic.sh

--- 00_Reads_processing/04_hic_reads_statistic.sh
+++ 00_Reads_processing/04_hic_reads_statistic.sh
@@ -5,4 +5,4 @@ hic=/public/caiyuanshi/hifi/hic/ANNO_ANCGD180475_PM-ANCGD180475-17_2022-07-06_17
 source /public/caiyuanshi/miniconda3/bin/activate seqkit
 
 ##### simple statistics of FASTA/Q files #####
-seqkit stats -a -j 2 $hic/FPH-2_R1.fq.gz $hic/FPH-2_R2.fq.gz > hicreads_stats.txt
+seqkit stats -a -j 2 $hic/FPH-2_R1.fq.gz $hic/FPH-2_R2.fq.gz > hic_stats.txt
