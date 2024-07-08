bbdfd416cd16ca01abda5bc02216f3d067c902d5
Enosh <79687590+yshcai@users.noreply.github.com>
2024-07-08 11:29:34 +0800

    Update 01_extract_ccs_reads.sh

--- 01_Genome_assembly/01_extract_ccs_reads.sh
+++ 01_Genome_assembly/01_extract_ccs_reads.sh
@@ -2,6 +2,9 @@
 rawdata_dir=/public/caiyuanshi/hifi/of_genome/0.rawdata/Axy-FP283/r64030_20220318_092639/
 ccsdata_dir=/public/caiyuanshi/hifi/of_genome/1.HiFireads
 
+##### activate conda environment #####
 source /public/caiyuanshi/miniconda3/bin/activate pbccs
+
+##### extract ccs reads #####
 nohup ccs ${rawdata_dir}/P05DY22557029-1_r64030_20220318_092639_3_H01.subreads.bam ${ccsdata_dir}/Of_FP283.ccs.fastq.gz --min-passes 3 \
 --report-file ccs_report.txt --log-file ccs.log -j 0 &
