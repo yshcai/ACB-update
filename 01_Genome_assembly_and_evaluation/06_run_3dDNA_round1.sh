3bdd4e708f212313b7202d34cf63475a54f4e4f3
Enosh <79687590+yshcai@users.noreply.github.com>
2024-07-08 19:51:54 +0800

    Update 06_run_3dDNA_round1.sh

--- 01_Genome_assembly_and_evaluation/06_run_3dDNA_round1.sh
+++ 01_Genome_assembly_and_evaluation/06_run_3dDNA_round1.sh
@@ -1,5 +1,5 @@
 # !/bin/bash
-juicer=/public/caiyuanshi/hifi/t2t_of/2.Assembly/contig_fasta/hifi_only_s0.25/juicer
+juicer=/public/caiyuanshi/hifi/t2t_of/2.Assembly/contig_fasta/hifi_only_s0.25/final_assembly/juicer
 
 ##### activate conda environment #####
 source /public/caiyuanshi/miniconda3/bin/activate juicer
@@ -11,5 +11,5 @@ fi
 references=$juicer/references
 chr=${juicer%/*}/3dDNA
 cd $chr
-/public/caiyuanshi/bioinfo_software/3dDNA/3d-dna-master/run-asm-pipeline.sh --input 10000 --early-exit \
+/public/caiyuanshi/bioinfo_software/3dDNA/3d-dna-master/run-asm-pipeline.sh --early-exit \
 $references/${fasta##*/} $juicer/work/FPH2/aligned/merged_nodups.txt &>3dDNA_run.log
