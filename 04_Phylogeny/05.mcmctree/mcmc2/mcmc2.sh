88e54dd80430610c7fd71dc37820632c52a5735d
Enosh <79687590+yshcai@users.noreply.github.com>
2024-07-09 16:14:23 +0800

    Create mcmc2.sh
    run MCMCTree with the approximate method

---
+++ 04_Phylogeny/05.mcmctree/mcmc2/mcmc2.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+##### activate conda environment #####
+source /share/home/nxy1hj2cys/miniconda3/bin/activate evolution
+
+##### run MCMCTree with the approximate method #####
+mcmctree mcmctree.ctl
