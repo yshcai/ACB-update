819cdb077501bcd8ab9ce792e52c476751f0b792
Enosh <79687590+yshcai@users.noreply.github.com>
2024-07-09 15:24:04 +0800

    Update 02_msa.sh

--- 04_Phylogeny/02_msa.sh
+++ 04_Phylogeny/02_msa.sh
@@ -5,6 +5,7 @@ fa=/share/home/nxy1hj2cys/hifi/t2t_of/5.Phylogeny/01.OrthoFinder2/02.output/Resu
 source /public/caiyuanshi/miniconda3/bin/activate evolution
 
 ##### perform multiple alignment #####
+mkdir 02.Mafft && cd 02.Mafft
 for i in `ls $fa/*.fa`;
 do
         mafft --thread 16 --auto $i > $(basename $i).mafft.aln
